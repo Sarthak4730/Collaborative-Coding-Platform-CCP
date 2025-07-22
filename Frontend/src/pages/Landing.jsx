e15d8385db09df8af00df545c40072ea1ad1c47a
github Sarthak4730 <sarthakasm07@gmail.com>
2025-07-22 21:50:09 +0530

    removed hover, it is breaking.

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -68,7 +68,7 @@ export default function Landing() {
                         <h4 className="font-bold text-2xl">Built-in chatbox.</h4>
                         <p>Text chat with room members.</p>
                     </div>
-                    <img className="transition-transform duration-300 ease-in-out hover:scale-110" src="/Chatbox.png" alt="pic" />
+                    <img src="/Chatbox.png" alt="pic" />
                 </Tilt>
 
                 <Tilt
@@ -84,7 +84,7 @@ export default function Landing() {
                         <h4 className="font-bold text-2xl">Real-time collaboration.</h4>
                         <p>Code together, see each other's cursors live.</p>
                     </div>
-                    <img className="transition-transform duration-300 ease-in-out hover:scale-110" src="/Cursors.png" alt="pic" />
+                    <img src="/Cursors.png" alt="pic" />
                 </Tilt>
 
                 <Tilt
@@ -100,7 +100,7 @@ export default function Landing() {
                         <h4 className="font-bold text-2xl">Multi-language support.</h4>
                         <p>C++, Java, & Python - compile & run instantly.</p>
                     </div>
-                    <img className="transition-transform duration-300 ease-in-out hover:scale-110" src="/Languages.png" alt="pic" />
+                    <img src="/Languages.png" alt="pic" />
                 </Tilt>
 
                 <Tilt
@@ -116,7 +116,7 @@ export default function Landing() {
                         <h4 className="font-bold text-2xl">Secure room access.</h4>
                         <p>Login protected and JWT-authenticated rooms.</p>
                     </div>
-                    <img className="transition-transform duration-300 ease-in-out hover:scale-110" src="/Login.png" alt="pic" />
+                    <img src="/Login.png" alt="pic" />
                 </Tilt>
             </div>
 
