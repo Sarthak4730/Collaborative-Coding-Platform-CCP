67d5f24c19a20a1afb44e38ca9008956ad22bb1c
github Sarthak4730 <sarthakasm07@gmail.com>
2025-09-13 11:23:42 +0530

    navbar done

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -24,25 +24,25 @@ const Navbar = () => {
 
     return <nav className="navbar h-1/12 md:h-[15vh] bg-gradient-to-r from-[#3b82f6] to-[#ef4444] text-white font-bold flex justify-between px-[2vw] items-center">
         <div className="left flex w-1/2 items-center gap-2">
-            <button className="w-1/4 h-1/4 md:w-[3vw] md:h-[3vw]" onClick={ () => navigate('/') }>
+            <button className="w-1/6 h-1/6 md:w-[3vw] md:h-[3vw]" onClick={ () => navigate('/') }>
                 <img src="/logo.svg" alt="logo" className="cursor-pointer hover:scale-110 transition border-2 border-white rounded-lg"/>
             </button>
-            <h1 className="text-sm md:text-2xl">Collaborative Coding Platform</h1>
+            <h1 className="w-1/2 text-sm md:text-2xl">Collaborative Coding Platform</h1>
         </div>
 
-        <div className="right flex justify-end w-1/2 bg-amber-300 items-center gap-4">
+        <div className="right flex justify-end md:justify-end w-1/2 h-full items-center md:gap-4">
             <div className="two-btns md:flex">
                 <div className="hover:scale-110 transition flex w-1/4 md:w-[7vw] justify-evenly">
                     <FaGithub className="text-2xl"/>
-                    <h3><a className="hover:text-blue-300" href="https://github.com/Sarthak4730/Collaborative-Coding-Platform-CCP" target="_blank">Github</a></h3>
+                    <h3><a className="hover:text-blue-300 underline md:no-underline" href="https://github.com/Sarthak4730/Collaborative-Coding-Platform-CCP" target="_blank">Github</a></h3>
                 </div>
                 <div className="hover:scale-110 transition flex w-1/4 md:w-[7vw] justify-evenly">
                     <IoIosMail className="text-2xl"/>
-                    <a className="hover:text-blue-300" href="mailto:sarthak.kharade.dev@gmail.com">Contact</a>
+                    <a className="hover:text-blue-300 underline md:no-underline" href="mailto:sarthak.kharade.dev@gmail.com">Contact</a>
                 </div>
             </div>
             {
-                isLoggedIn && <button onClick={handleLogout} className="w-1/4 md:w-[7vw] h-[7vh] rounded-xl cursor-pointer text-lg transition hover:scale-110 hover:text-blue-500 text-red-500 font-bold border-2 border-black bg-white">Logout</button>
+                isLoggedIn && <button onClick={handleLogout} className="w-1/3 h-1/2 md:w-[7vw] md:h-[7vh] rounded-xl cursor-pointer text-sm md:text-lg transition hover:scale-110 hover:text-blue-500 text-red-500 font-bold border-2 border-black bg-white">Logout</button>
             }
         </div>
     </nav>
