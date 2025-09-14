fc216aaec119f55e454fda58b5c5a06ca4ebb3d7
github Sarthak4730 <sarthakasm07@gmail.com>
2025-09-14 12:48:31 +0530

    Login & Register pages done.

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -42,22 +42,22 @@ export default function Login() {
     }
     
     return <>
-        <form onSubmit={handleSubmit} className="bg-gray-200 w-[50vw] h-[75vh] mx-auto mt-[10vh] rounded-2xl flex flex-col items-center justify-evenly">
-            <h1 className="text-3xl font-bold">Log in to existing account</h1>
+        <form onSubmit={handleSubmit} className="bg-gray-200 w-5/6 h-96 md:w-[50vw] md:h-[75vh] mx-auto mt-64 md:mt-[10vh] rounded-2xl flex flex-col items-center justify-evenly">
+            <h1 className="text-center text-2xl md:text-3xl font-bold">Log in to existing account</h1>
 
-            <input className="border-2 border-blue-500 bg-white rounded-2xl pl-4 h-10 w-[20vw]" type="email" name="email" placeholder="Enter Email" onChange={handleChange} required/>
-            <div className="password-div flex relative">
-                <input className="border-2 border-blue-500 bg-white rounded-2xl pl-4 h-10 w-[20vw]" type={isHidden ? "password" : "text"} name="password" placeholder="Enter Password" onChange={handleChange} required/>
-                {isHidden ? <LuEye className="cursor-pointer absolute right-4 top-2 text-2xl" onClick={() => setIsHidden(!isHidden)}/> : <LuEyeClosed className="cursor-pointer absolute right-4 top-2 text-2xl" onClick={() => setIsHidden(!isHidden)}/>}
+            <input className="border-2 border-blue-500 bg-white rounded-2xl pl-4 w-2/3 py-1 md:h-10 md:w-[20vw]" type="email" name="email" placeholder="Enter Email" onChange={handleChange} required/>
+            <div className="password-div w-5/6 md:w-auto flex relative">
+                <input className="border-2 border-blue-500 bg-white rounded-2xl pl-4 py-1 ml-8 md:ml-auto w-4/5 md:h-10 md:w-[20vw]" type={isHidden ? "password" : "text"} name="password" placeholder="Enter Password" onChange={handleChange} required/>
+                {isHidden ? <LuEye className="cursor-pointer absolute right-9.5 top-1.5 md:right-4 md:top-2 text-2xl" onClick={() => setIsHidden(!isHidden)}/> : <LuEyeClosed className="cursor-pointer absolute right-9.5 top-1.5 md:right-4 md:top-2 text-2xl" onClick={() => setIsHidden(!isHidden)}/>}
             </div>
 
             {error && <p className="text-lg font-bold text-red-600">*{error}*</p>}
 
-            <button type="submit" className="w-[12.5vw] h-[7.5vh] rounded-xl cursor-pointer text-lg hover:scale-105 font-bold text-white bg-blue-500 ">Login</button>
+            <button type="submit" className="w-1/3 md:w-[12.5vw] md:h-[7.5vh] rounded-xl cursor-pointer py-1 text-lg hover:scale-105 font-bold text-white bg-blue-500 ">Login</button>
         
-            <div className="login-div flex items-center gap-3">
-                <p className="text-lg font-semibold">Don't have an account?</p>
-                <button onClick={ () => { navigate("/register") } } type="submit" className="w-[12.5vw] h-[7.5vh] rounded-xl cursor-pointer text-lg hover:scale-105 font-bold border-3 border-blue-500 ">Register</button>
+            <div className="login-div w-5/6 flex justify-center gap-3 items-center">
+                <p className="text-xs md:text-lg font-semibold">Don't have an account?</p>
+                <button onClick={ () => { navigate("/register") } } type="submit" className="w-1/3 md:w-[12.5vw] md:h-[7.5vh] rounded-xl cursor-pointer py-1 text-lg hover:scale-105 font-bold border-3 border-blue-500 ">Register</button>
             </div>
         </form>
     </>
