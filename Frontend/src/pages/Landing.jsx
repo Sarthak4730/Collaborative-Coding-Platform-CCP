4c37e454c6da105a9daed8268e96f3d5a40b41e7
github Sarthak4730 <sarthakasm07@gmail.com>
2025-09-22 11:09:21 +0530

    mobile screen size blunder, landing page - can't touch buttons and the down double arrow not visible clearly

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -47,13 +47,24 @@ export default function Landing() {
                     autoplay
                 />
             </div>
-            <div className="absolute bottom-2 flex justify-center mt-10 animate-bounce text-white text-2xl">
+            <div className="absolute bottom-8 flex justify-center mt-10 animate-bounce text-white text-2xl">
                 <FaAngleDoubleDown style={ { height: "8vh", width: "8vw" } } />
             </div>
         </section>
 
         <section className="navbar-and-features-and-buttons h-screen snap-start">
             <Navbar />
+            
+            <div className="buttons h-1/6 md:h-[25vh] flex justify-center gap-[3vw] items-center">
+                <button onClick={() => handleClick("create")} className="w-1/3 md:w-[15vw] h-1/3 md:h-[10vh] rounded-xl cursor-pointer text-sm md:text-lg transition hover:scale-110 font-bold text-white bg-blue-500 flex justify-evenly md:justify-center md:gap-2 items-center">
+                    <IoCreateOutline className="text-2xl" />
+                    <p className="w-2/3">Create a Room</p>
+                </button>
+                <button onClick={() => handleClick("join")} className="w-1/3 md:w-[15vw] h-1/3 md:h-[10vh] rounded-xl cursor-pointer text-sm md:text-lg transition hover:scale-110 font-bold text-white bg-red-500 flex justify-evenly md:justify-center md:gap-2 items-center">
+                    <IoEnterOutline className="text-2xl" />
+                    <p className="w-2/3">Join a Room</p>
+                </button>
+            </div>
 
             <div className="features flex flex-col md:h-[60vh] md:grid md:grid-cols-2 md:grid-rows-2 px-[3vw] py-8 md:pt-[2vw] gap-8 md:gap-[3vw]">
                 <Tilt
@@ -119,17 +130,6 @@ export default function Landing() {
                     </div>
                     <img className="ml-4 md:w-auto h-full md:h-[20vh] transition hover:scale-130" src="/Login.png" alt="pic" />
                 </Tilt>
-            </div>
-
-            <div className="buttons h-1/6 md:h-[25vh] flex justify-center gap-[3vw] items-center">
-                <button onClick={() => handleClick("create")} className="w-1/3 md:w-[15vw] h-1/3 md:h-[10vh] rounded-xl cursor-pointer text-sm md:text-lg transition hover:scale-110 font-bold text-white bg-blue-500 flex justify-evenly md:justify-center md:gap-2 items-center">
-                    <IoCreateOutline className="text-2xl" />
-                    <p className="w-2/3">Create a Room</p>
-                </button>
-                <button onClick={() => handleClick("join")} className="w-1/3 md:w-[15vw] h-1/3 md:h-[10vh] rounded-xl cursor-pointer text-sm md:text-lg transition hover:scale-110 font-bold text-white bg-red-500 flex justify-evenly md:justify-center md:gap-2 items-center">
-                    <IoEnterOutline className="text-2xl" />
-                    <p className="w-2/3">Join a Room</p>
-                </button>
             </div>
         </section>
     </div>
